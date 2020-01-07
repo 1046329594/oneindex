b1b246096564669adcdcc83afff5a36fef87fbdd
hongchenhao <31385593+1046329594@users.noreply.github.com>
2020-01-07 22:11:43 +0800

    Delete cron.sh

--- cron.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-php /var/www/html/one.php cache:refresh
-cd /var/www/html/cache
-rm -rf .git
-git init
-git add .
-git commit -m "Update"
-git remote add origin git@github.com:1046329594/cache.git
-git push -f origin master
