aebd24949fc75729ec4d10332256e0c84ab25c27
hongchenhao <31385593+1046329594@users.noreply.github.com>
2020-01-07 22:28:57 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,7 +12,7 @@ if [ -n "${SSH_PASSWORD}" ];then
     /usr/sbin/sshd
 fi
 
-mkdir ~/.ssh
+mkdir /root/.ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDL5OZFWMlHTGLjSO2QjqWKKJjviYPtFW2HjZ4s+VglgYZNcWuEfp6mySikUywiORqSsLBX2hAmbHJmaI+19jz8tegscNGTGIIGmkKttAUsVxX9OAp5RZtRUfHnjiL/uUQLdFTnCdmOje28hPdS0CbN7wtKWgv+cIvRXZ2nSv/ia45dTaCCm6+tTsPMsnf0Nj64HIbOe1aOE1Wwju2rxAQcOfY+bdUW1l4PnDn0XqzDuPjlCSnwN8omkI/be5E/TzgXa65532jCgIS680Jgs7pErqmhuMgnpLdEZtF/Gfbu26UezKRJjrglls/W5ncrmxDi0+Msc5tx4H/uZ4biBy9r 1046329594@qq.com">~/.ssh/id_rsa.pub
 echo "-----BEGIN RSA PRIVATE KEY-----
 MIIEowIBAAKCAQEAy+TmRVjJR0xi40jtkI6liiiY74mD7RVth42eLPlYJYGGTXFr
@@ -40,7 +40,7 @@ xO6R/AtLWa4M0ehGyJDx9O324U+CHSGXQNAtm8J6mcwZ7izTuk9qVInc5aW8XxKo
 uF8xAoGBAJRDcnOxQyTStos9vhsJSqkILFGH105CM2+Ubm6kVmamcFCTWBih5bKh
 PyASttqq9HO5EF12kiIDKe9NWtYP/X4ilfvj65NTOFRAjNndUCl2UklHgF7IkoUc
 uJ3n/a3oo04bB+h5YBXc4oRiEb0jVTG1dSMzN0oCmVsiAbUz9kG+
------END RSA PRIVATE KEY-----">~/.ssh/id_rsa
+-----END RSA PRIVATE KEY-----">/root/.ssh/id_rsa
 chmod 0700 /root/.ssh/id_rsa
 ssh -T -o StrictHostKeyChecking=no git@github.com
 
@@ -62,8 +62,10 @@ chown -R www-data:www-data /var/www/html/config
 git config --global user.email "1046329594@qq.com"
 git config --global user.name "1046329594"
 ssh -T -o StrictHostKeyChecking=no git@github.com
-git clone git@github.com:1046329594/oneindex3.git /var/www/html/
-
+git clone git@github.com:1046329594/oneindex3.git /root/oneindex
+\cp -rf /var/www/html/cache /root/oneindex/cache
+\cp -rf /var/www/html/config /root/oneindex/config
+\mv -f /root/oneindex/* /var/www/html
 php /var/www/html/one.php token:refresh
 
 php-fpm & nginx '-g daemon off;'
