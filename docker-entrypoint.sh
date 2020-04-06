bae9389c3a2e71ba6bb0f0a987eb59c81312e88b
hongchenhao <31385593+StarOz@users.noreply.github.com>
2020-04-06 12:15:20 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-
-# timezone
 ln -sf /usr/share/zoneinfo/${TZ:-"Asia/Shanghai"} /etc/localtime
 echo ${TZ:-"Asia/Shanghai"} > /etc/timezone
-
 sed -i "s|listen 80|listen ${PORT:-80}|" /etc/nginx/conf.d/default.conf
 chown -R www-data:www-data /var/www/html/cache
 chown -R www-data:www-data /var/www/html/config
-
 php /var/www/html/one.php token:refresh
-if [ ! -f "/var/www/html/config/token.php" ];then
-    exit 1
-fi
-
 php-fpm & nginx '-g daemon off;'
